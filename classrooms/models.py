d2fff2754466991bb73981c9d468f0e3de208989
Kunj Panchal <kunjpanchal2006@gmail.com>
2025-12-02 00:43:13 +0530

    Made classrooms functional

--- classrooms/models.py
+++ classrooms/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Classroom(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    capacity = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
--- classrooms/views.py
+++ classrooms/views.py
@@ -5,16 +5,21 @@ from .models import Classroom
 
 def classroom_list(request):
     classrooms = Classroom.objects.all()
-    return render(request, 'classrooms/list.html', {'classrooms': classrooms})
+    return render(request, 'classrooms/classroom_list.html', 
+                  {'classrooms': classrooms,
+                   'total_classrooms': classrooms.count()
+                })
 
 def classroom_create(request):
     if request.method == 'POST':
         name = request.POST['name']
         description = request.POST.get('description', '')
-        Classroom.objects.create(name=name, description=description)
+        capacity = request.POST.get('capacity', 0)
+
+        Classroom.objects.create(name=name, description=description, capacity=capacity)
         return redirect('classroom_list')
     
-    return render(request, 'classrooms/create.html')
+    return render(request, 'classrooms/classroom_create.html')
 
 def classroom_edit(request, id):
     classroom = get_object_or_404(Classroom, id=id)
@@ -22,10 +27,11 @@ def classroom_edit(request, id):
     if request.method == 'POST':
         classroom.name = request.POST['name']
         classroom.description = request.POST.get('description', '')
+        classroom.capacity = request.POST.get('capacity', 0)
         classroom.save()
         return redirect('classroom_list')
 
-    return render(request, 'classrooms/edit.html', {'classroom': classroom})
+    return render(request, 'classrooms/classroom_edit.html', {'classroom': classroom})
 
 def classroom_delete(request, id):
     classroom = get_object_or_404(Classroom, id=id)
