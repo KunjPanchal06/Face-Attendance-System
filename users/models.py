81a1ca31d0d145bd39fc0507731eea104028064b
Kunj Panchal <kunjpanchal2006@gmail.com>
2025-12-01 22:42:27 +0530

    Added UserRole + StudentProfile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserRole(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('student', 'Student'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    face_image = models.ImageField(upload_to='face_images/', null=True, blank=True)
+    embedding = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
