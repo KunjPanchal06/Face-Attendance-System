cb833d1b66bdde09eefe54bf552a45559a23ba81
Kunj Panchal <kunjpanchal2006@gmail.com>
2025-12-24 21:45:15 +0530

    Completed face registration and attendance matching backend

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import mark_attendance_view
+from .views import mark_attendance_view, test_attendance_page
 
 urlpatterns = [
     path("mark/", mark_attendance_view, name="mark_attendance"),
+    path("test/", test_attendance_page),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from students.face_utils import generate_embedding
 from attendance.face_matcher import find_matching_student
 from attendance.services import mark_attendance
+from django.shortcuts import render
 
 def mark_attendance_view(request):
     if request.method == "POST" and request.FILES.get("image"):
@@ -23,3 +24,20 @@ def mark_attendance_view(request):
         return JsonResponse({"status": "no_match"}, status=404)
 
     return JsonResponse({"error": "Invalid request"}, status=400)
+
+def test_attendance_page(request):
+    result = None
+
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+
+        embedding = generate_embedding(image)
+        student, similarity = find_matching_student(embedding)
+
+        if student:
+            attendance, created = mark_attendance(student)
+            result = f"Matched {student.roll_no}, similarity={similarity:.2f}"
+        else:
+            result = "No match found"
+
+    return render(request, "attendance_test.html", {"result": result})
